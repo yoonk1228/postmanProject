8903eba77eb07495293fb338d22d2360ee503289
bbb4756 <rlarbfl4756@gmail.com>
2022-02-15 15:10:17 +0900

    프로필,회원가입

--- routers/user/index.js
+++ routers/user/index.js
@@ -1,4 +1,25 @@
+
 const express = require('express')
 const router = express.Router()
 
-module.exports = router
+//user 규리
+
+router.get('user/profile',(req,res)=>{
+    console.log('프로필보기')
+})
+
+router.get('user/signup',(req,res)=>{
+    console.log('회원가입')
+})
+
+router.post('user/signup',(req,res)=>{
+    console.log('회원가입 완료')
+})
+
+router.get('user/update',(req,res)=>{
+    console.log('회원가입 정보 수정')
+})
+
+router.post('user/update',(req,res)=>{
+    console.log('회원가입 정보 수정 완료')
+})
