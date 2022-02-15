08f2d34eafd1edc554fcb2d8d828023354833671
kimhelp <operat8009@gmail.com>
2022-02-15 14:54:30 +0900

    글보기~글쓰기

--- routers/board/index.js
+++ routers/board/index.js
@@ -1,4 +1,22 @@
 const express = require('express')
 const router = express.Router()
 
+//김도움
+router.get('board/view',(req,res)=>{
+    console.log('글보기 get 확인');
+})
+router.get('board/update',(req,res)=>{
+    console.log('글수정 get 확인');
+})
+router.post('board/update',(req,res)=>{
+    console.log('글수정 post 확인');
+})
+router.get('board/write',(req,res)=>{
+    console.log('글쓰기 get 확인');
+})
+router.post('board/write',(req,res)=>{
+    console.log('글쓰기 post 확인');
+})
+
+
 module.exports = router
--- routers/index.js
+++ routers/index.js
@@ -1,9 +1,14 @@
 const express = require('express')
 const router = express.Router()
+//김도움
+const boardRouter = require('./board')
 
 router.get('/',(req,res)=>{
   console.log("'/로 get 요청'")
 })
+
+//김도움
+router.use('/board',boardRouter)
 
 // 이걸 안적으면, 어디에서도 이 index.js를 불러오지못해요.
 module.exports = router
