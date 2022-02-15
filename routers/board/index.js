f2c30e600276686a42fdc27f9abae3507e1676cf
yoonk1228 <yoonk1228@naver.com>
2022-02-15 15:40:16 +0900

    hihihi를 board/view로 확인 가능

--- routers/board/index.js
+++ routers/board/index.js
@@ -4,6 +4,7 @@ const router = express.Router()
 //김도움
 router.get('/view',(req,res)=>{
     console.log('글보기 get 확인');
+    res.send('hihihi')
 })
 router.get('/update',(req,res)=>{
     console.log('글수정 get 확인');
